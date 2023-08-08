e334e9a3654f0d6858fde718d3b9acdabadc103f
Madhav Paliwal <madhavpaliwal02@gmail.com>
2023-08-08 19:45:28 +0530

    Implementing React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Base from './components/Base';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
 
 function App() {
   return (
-    <Base>
-      <h1>This is Home Page</h1>
-    </Base>
+    // <Base>
+    //   <h1>This is Home Page</h1>
+    // </Base>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
