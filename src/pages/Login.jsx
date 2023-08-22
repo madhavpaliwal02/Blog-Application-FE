99d1c590739f0de5ab59f3b3b4aa95ab5cdd5fe3
Madhav <madhavpaliwal02@gmail.com>
2023-08-22 12:33:40 +0530

    Signup & Login Form implemented

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,57 @@
 import React from 'react'
 import Base from '../components/Base'
+import { Button, Card, CardBody, CardHeader, Col, Container, Form, FormGroup, Input, Label, Row } from 'reactstrap'
 
 const Login = () => {
     return (
         <Base>
-            <div>
-                <h1>This is login page</h1>
-            </div>
+            <Row>
+                <Col sm={{ size: 4, offset: 4 }}>
+                    <Container className='mt-3'>
+
+                        <Card color='dark' inverse>
+                            <CardHeader className='text-center'>
+                                <h2>Blog Application LOGIN Form !!</h2>
+                            </CardHeader>
+                            <CardBody>
+                                <Form>
+                                    {/* Email */}
+                                    <FormGroup>
+                                        <Label for='email'>
+                                            Enter Email
+                                        </Label>
+                                        <Input
+                                            id='email'
+                                            name='email'
+                                            type='email'
+                                            placeholder='Enter here'
+                                        />
+                                    </FormGroup>
+                                    {/* Password */}
+                                    <FormGroup>
+                                        <Label for='password'>
+                                            Enter Password
+                                        </Label>
+                                        <Input
+                                            id='password'
+                                            name='password'
+                                            type='password'
+                                            placeholder='Enter here'
+                                        />
+                                    </FormGroup>
+
+                                    <Container className='text-center'>
+                                        <Button type='submit' color='info'>Submit</Button>
+                                        <Button type='reset' color='info' className='ms-2'>Reset</Button>
+                                    </Container>
+                                </Form>
+                            </CardBody>
+                        </Card>
+
+
+                    </Container>
+                </Col>
+            </Row>
         </Base>
     )
 }
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,12 +1,81 @@
 import React from 'react'
 import Base from '../components/Base'
+import { Button, Card, CardBody, CardHeader, Col, Container, Form, FormGroup, Input, Label, Row } from 'reactstrap'
 
 const Signup = () => {
     return (
         <Base>
-            <div>
-                <h1>Signup</h1>
-            </div>
+            <Container>
+
+                <Row>
+                    <Col sm={{ size: 6, offset: 3 }} >
+
+                        <Card color='dark' inverse className='mt-2'>
+                            {/* Header */}
+                            <CardHeader>
+                                <h2>Blog Application SIGN UP Form !!</h2>
+                            </CardHeader>
+
+                            <CardBody>
+
+                                {/* Sign Up Form */}
+                                <Form>
+                                    {/* Name */}
+                                    <FormGroup>
+                                        <Label for='name' >Enter Name</Label>
+                                        <Input
+                                            id='name'
+                                            name='name'
+                                            type='text'
+                                            placeholder='Enter here...'
+                                        />
+                                    </FormGroup>
+                                    {/* Email */}
+                                    <FormGroup>
+                                        <Label for='email' >Enter Email</Label>
+                                        <Input
+                                            id='email'
+                                            name='email'
+                                            type='email'
+                                            placeholder='Enter here...'
+                                        />
+                                    </FormGroup>
+                                    {/* Password */}
+                                    <FormGroup>
+                                        <Label for='password' >Enter Password</Label>
+                                        <Input
+                                            id='password'
+                                            name='password'
+                                            type='password'
+                                            placeholder='Enter here...'
+                                        />
+                                    </FormGroup>
+                                    {/* About */}
+                                    <FormGroup>
+                                        <Label for='about' >Enter About</Label>
+                                        <Input
+                                            id='about'
+                                            name='about'
+                                            type='textarea'
+                                            placeholder='Enter here...'
+                                        />
+                                    </FormGroup>
+
+                                    <Container className='text-center'>
+                                        <Button type='' color='info'>Submit</Button>
+                                        <Button type='reset' color='info' className='ms-3'>Reset</Button>
+                                    </Container>
+                                </Form>
+
+                            </CardBody>
+                        </Card>
+                    </Col>
+
+                </Row>
+
+            </Container>
+
+
         </Base>
     )
 }
